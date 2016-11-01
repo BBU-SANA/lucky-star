53ab298f7ff27ce002052924cad2481142cde31c
sopheakreth <rethsopheak@gmail.com>
2016-11-01 19:48:44 +0700

    upload

---
+++ application/views/admin/layouts/footer.php
@@ -0,0 +1,40 @@
+
+		<footer class="footer text-right">
+            © 2016. All rights reserved.
+        </footer>
+
+    </div>
+    
+    <!-- ============================================================== -->
+    <!-- End Right content here -->
+    <!-- ============================================================== -->
+
+</div>
+<!-- END wrapper -->
+
+	<script>
+	    var resizefunc = [];
+	</script>
+
+	<!-- jQuery  -->
+	<script src="<?=base_url();?>/public/assets/js/jquery.min.js"></script>
+	<script src="<?=base_url();?>/public/assets/js/bootstrap.min.js"></script>
+	<script src="<?=base_url();?>/public/assets/js/detect.js"></script>
+	<script src="<?=base_url();?>/public/assets/js/fastclick.js"></script>
+	<script src="<?=base_url();?>/public/assets/js/jquery.slimscroll.js"></script>
+	<script src="<?=base_url();?>/public/assets/js/jquery.blockUI.js"></script>
+	<script src="<?=base_url();?>/public/assets/js/waves.js"></script>
+	<script src="<?=base_url();?>/public/assets/js/wow.min.js"></script>
+	<script src="<?=base_url();?>/public/assets/js/jquery.nicescroll.js"></script>
+	<script src="<?=base_url();?>/public/assets/js/jquery.scrollTo.min.js"></script>
+
+	<script src="<?=base_url();?>/public/assets/js/jquery.core.js"></script>
+	<script src="<?=base_url();?>/public/assets/js/jquery.app.js"></script>
+
+	<script src="<?=base_url();?>/public/assets/plugins/morris/morris.min.js"></script>
+	<script src="<?=base_url();?>/public/assets/plugins/raphael/raphael-min.js"></script>
+
+	<script src="<?=base_url();?>/public/assets/pages/jquery.dashboard_ecommerce.js"></script>
+
+</body>
+</html>
---
+++ application/views/admin/layouts/slidemenu.php
@@ -0,0 +1,69 @@
+
+    <!-- ========== Left Sidebar Start ========== -->
+
+    <div class="left side-menu">
+        <div class="sidebar-inner slimscrollleft">
+            <!--- Divider -->
+            <div id="sidebar-menu">
+                <ul>
+
+                	<!-- <li class="text-muted menu-title">Navigation</li> -->
+
+			        <li class="has_sub">
+                        <a href="javascript:void(0);" class="waves-effect active"><i class="ti-home"></i> <span> Dashboard </span> <span class="menu-arrow"></span></a>
+                        <ul class="list-unstyled">
+                            <li><a href="http://localhost:2222/lucky-star-computer/" target="_new">View Page</a></li>
+                        </ul>
+                    </li>
+
+                    <li class="has_sub">
+                        <a href="javascript:void(0);" class="waves-effect"><i class="ti-user"></i> <span> Users </span> <span class="menu-arrow"></span></a>
+                        <ul class="list-unstyled">
+                            <li><a href="index-2.html">Users List</a></li>
+                            <li><a href="dashboard_2.html">Add Users</a></li>
+                        </ul>
+                    </li>
+
+                    <li class="has_sub">
+                        <a href="javascript:void(0);" class="waves-effect"><i class="ti-pencil-alt"></i> <span> Categories </span> <span class="menu-arrow"></span></a>
+                        <ul class="list-unstyled">
+                            <li><a href="index-2.html">Categories List</a></li>
+                            <li><a href="dashboard_2.html">Brands List</a></li>
+                            <li><a href="dashboard_2.html">Add New</a></li>
+                        </ul>
+                    </li>
+
+                    <li class="has_sub">
+                        <a href="javascript:void(0);" class="waves-effect"><i class=" ti-briefcase"></i> <span> Products </span> <span class="menu-arrow"></span></a>
+                        <ul class="list-unstyled">
+                            <li><a href="index-2.html">Products List</a></li>
+                            <li><a href="dashboard_2.html">Add Products</a></li>
+                        </ul>
+                    </li>
+
+                    <li class="has_sub">
+                        <a href="javascript:void(0);" class="waves-effect"><i class="ti-shopping-cart"></i> <span> Selling </span> <span class="menu-arrow"></span></a>
+                        <ul class="list-unstyled">
+                            <li><a href="index-2.html">Selling List</a></li>
+                            <li><a href="dashboard_2.html">Add Selling</a></li>
+                        </ul>
+                    </li>
+
+                    <li class="has_sub">
+                        <a href="javascript:void(0);" class="waves-effect"><i class=" ti-check-box"></i> <span> Sell History </span></a>
+                    </li>
+
+                    <li class="has_sub">
+                        <a href="javascript:void(0);" class="waves-effect"><i class=" ti-receipt"></i> <span> Receipts </span> <span class="menu-arrow"></span></a>
+                        <ul class="list-unstyled">
+                            <li><a href="index-2.html">Orders List</a></li>
+                            <li><a href="dashboard_2.html">Add Receipt</a></li>
+                        </ul>
+                    </li>
+                </ul>
+                <div class="clearfix"></div>
+            </div>
+            <div class="clearfix"></div>
+        </div>
+    </div>
+    <!-- Left Sidebar End --> 
